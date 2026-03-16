74c4f11b25498ec9973daaacdc101ef205693f16
LL66o <141141627+caoliang520@users.noreply.github.com>
2026-03-16 21:31:06 +0800

    Create temp.java

---
+++ temp.java
@@ -0,0 +1,14 @@
+/**
+ * 基础版 Java HelloWorld
+ * 编译运行步骤：
+ * 1. 保存为 HelloWorld.java
+ * 2. 编译：javac HelloWorld.java
+ * 3. 运行：java HelloWorld
+ */
+public class HelloWorld {
+    // 程序入口方法（main方法是Java程序的执行起点）
+    public static void main(String[] args) {
+        // 控制台输出 Hello World
+        System.out.println("Hello, World!");
+    }
+}
